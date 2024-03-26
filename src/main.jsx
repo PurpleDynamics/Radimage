874b68aed8a93dc84677b8409e362f4613cb32fb
yunshin <2younsin@gmail.com>
2024-03-26 23:53:18 +0900

    init : Raimage 프로젝트 개시

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import Text from "./pages/text";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Text />
+    </React.StrictMode>
+);
